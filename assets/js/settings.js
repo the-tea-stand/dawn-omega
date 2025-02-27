ec3eba43488c640cafd66fc10267114133776a4f
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2025-02-27 16:01:16 -0500

    volunteers

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -66,7 +66,7 @@ const signupDescription = `
 <div>
 The Tea Stand is 
 <a href='/a-note-from-miles' class='link'>my</a> 
-full-time work and main source of income.
+full-time work and is supported by many volunteers and comrades.
 </div>
 <div>
 We created this membership program with the goal of sustaining The Tea Stand as an anti-capitalist, community-led project. Thank you for enabling us to host <a href='/calendar' class='link'>free events</a>, serve free tea, and cultivate neighborhood trust.
