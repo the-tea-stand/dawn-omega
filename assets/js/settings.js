1f3da173131c3237beaa8d67e809cc9973a49d8d
Adin <akvashi24@gmail.com>
2025-05-05 15:05:11 -0400

    Padding changes

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,18 +1,20 @@
 // Homepage text
-// 
+//
 // Notes:
 // Each of these variables should be a string.  It should be well-formed HTML.
 // This is a site to test your HTML: https://onecompiler.com/html/
 // It can also just be a regular string, e.g. "This is a string"
 
+const description =
+  "<span>We serve free tea to build trust<br/>and community around Brooklyn.<br/><br/>To date, we've served 5,468<br/>free cups and counting 🍵</span>";
 
-const description = "<span>We serve free tea to build trust<br/>and community around Brooklyn.<br/><br/>To date, we've served 5,468<br/>free cups and counting 🍵</span>"
+const articlesTitle = "Our Newsletter";
 
-const articlesTitle = "Our Newsletter"
+const programmingSubtitle =
+  "serving free tea at parks, food distributions, and events we host";
 
-const programmingSubtitle = "serving free tea at parks, food distributions, and events we host"
-
-const newsletterSubtitle = "monthly reflections on what it's like to run The Tea Stand"
+const newsletterSubtitle =
+  "monthly reflections on what it's like to run The Tea Stand";
 
 // Modality cards on homepage
 //
@@ -20,56 +22,62 @@ const newsletterSubtitle = "monthly reflections on what it's like to run The Tea
 // Make sure cards.url is a full link!
 // Notice those quotations
 // Hit me if you have any questions
-// 
+//
 // e.g.
 // const cards = [
 //     {
 //         title: 'Distros',
 //         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
-//         url: 'http://theteastand.org/distros/' 
+//         url: 'http://theteastand.org/distros/'
 //     },
 //     {
 //         title: 'Popups',
 //         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
-//         url: 'https://theteastand.org/popups/' 
+//         url: 'https://theteastand.org/popups/'
 //     },
 // ]
 
 const cards = [
-    {
-        title: "⛩️ Popups",
-        url: "https://www.theteastand.org/popups/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/popup.JPG?raw=true"
-    },
-    {
-        title: "🥬 Distros",
-        url: "https://www.theteastand.org/distros/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/distro.jpg?raw=true"
-    },
-    {
-        title: "🫖 Tea Talks",
-        url: "https://www.theteastand.org/tea-talks/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/tea-talk.JPG?raw=true"
-    },
-    {
-        title: "📀 Steeped in Sound",
-        url: "https://www.theteastand.org/steeped-in-sound/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/steeped.jpeg?raw=true"
-    },
-    {
-        title: "🪷 Silent Sanctuary",
-        url: "https://www.theteastand.org/silent-sanctuary/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/silent-sanctuary.jpg?raw=true"
-    },
-    {
-        title: "🌀 Perpetual Brew",
-        url: "https://www.theteastand.org/perpetual-brew/",
-        image: "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/perpetual-brew.jpg?raw=true"
-    },
-]
+  {
+    title: "⛩️ Popups",
+    url: "https://www.theteastand.org/popups/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/popup.JPG?raw=true",
+  },
+  {
+    title: "🥬 Distros",
+    url: "https://www.theteastand.org/distros/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/distro.jpg?raw=true",
+  },
+  {
+    title: "🫖 Tea Talks",
+    url: "https://www.theteastand.org/tea-talks/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/tea-talk.JPG?raw=true",
+  },
+  {
+    title: "📀 Steeped in Sound",
+    url: "https://www.theteastand.org/steeped-in-sound/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/steeped.jpeg?raw=true",
+  },
+  {
+    title: "🪷 Silent Sanctuary",
+    url: "https://www.theteastand.org/silent-sanctuary/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/silent-sanctuary.jpg?raw=true",
+  },
+  {
+    title: "🌀 Perpetual Brew",
+    url: "https://www.theteastand.org/perpetual-brew/",
+    image:
+      "https://github.com/the-tea-stand/dawn-omega/blob/main/assets/images/perpetual-brew.jpg?raw=true",
+  },
+];
 
 // Signup Page Text
-// 
+//
 // You can use template literals (with `ticks`) to make multiline strings.
 
 const signupDescription = `
@@ -87,4 +95,4 @@ Perks are offered as an act of reciprocity and are not meant to equal the dollar
 <div>
 Thank you for enabling us to host <a href='/calendar' class='link'>free events</a>, serve free tea, and create spaces of stillness and trust.
 </div>
-`
+`;
