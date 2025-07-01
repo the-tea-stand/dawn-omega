34f4ff800f73f04029e863ab93bbd651981f5dfe
Adin <akvashi24@gmail.com>
2025-07-01 19:38:11 -0400

    Add checks

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -2,14 +2,17 @@ const populateSignupText = () => {
   console.debug("Setting signup text...");
   if (signupDescription && signupDescription.length) {
     const section = document.getElementById("signup-description");
-    section.innerHTML += signupDescription;
+    if (section) {
+      section.innerHTML += signupDescription;
+    }
   }
   console.debug("Setting membership text...");
   if (membersDescription && membersDescription.length) {
     console.log(membersDescription);
     const section = document.getElementById("members-description");
-    console.log(section);
-    section.innerHTML += membersDescription;
+    if (section) {
+      section.innerHTML += membersDescription;
+    }
   }
 };
 
