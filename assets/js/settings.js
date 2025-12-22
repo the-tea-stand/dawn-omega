e846d99401bd967a297b18a7f0b46f4b5ccc2806
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2025-12-22 12:09:02 -0500

    update tea count

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -10,7 +10,7 @@
 // It can also just be a regular string, e.g. "This is a string"
 
 const description =
-  "<span>We serve free tea to build trust<br/>and community around Brooklyn.<br/><br/>To date, we've served 8,885<br/>free cups and counting!</span>";
+  "<span>We serve free tea to build trust<br/>and community around Brooklyn.<br/><br/>To date, we've served 9,151<br/>free cups and counting!</span>";
 
 const articlesTitle = "Our Newsletter";
 
