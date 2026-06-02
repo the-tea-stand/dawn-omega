64baa24c79315db6d3789bc61425ee6ba754b448
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2026-06-02 12:09:19 -0400

    artisan typo fuck

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -232,7 +232,7 @@ let membershipTiers = [
         state: "bold",
         title: "🫖 Handmade Teapot",
         subtitle:
-          "An artisinal 'easy gaiwan' teapot made by local ceramicist Lilian Wu.",
+          "An artisanal 'easy gaiwan' teapot made by local ceramicist Lilian Wu.",
       },
       {
         state: "bold",
