4804b0ad258ce955bfef5f744fd0d082b7c3f1ac
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2026-08-13 16:05:19 -0400

    add line breaks

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -134,10 +134,10 @@ const newsletterSignupDescription = `
 Our monthly newsletter is forever free and the best way to stay updated on all Tea Stand happenings. Long term, we plan to leave social media and rely on email for all communications.
 </div>
 <div>
-Each newsletter features upcoming events, housekeeping notes, and deeper dives into our programming and philosophy.
+<br>Each newsletter features upcoming events, housekeeping notes, and deeper dives into our programming and philosophy.
 </div>
 <div>
-Scroll down to explore our past newsletters.
+<br>Scroll down to explore our past newsletters.
 </div>`;
 
 // Membership Tiers and Perks
