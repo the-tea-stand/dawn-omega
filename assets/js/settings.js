1e9bfc4cce60ec69149a2f0ed503e5a09968106d
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2026-09-21 15:25:44 -0400

    unique gaiwan

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -256,7 +256,7 @@ let membershipTiers = [
         state: "bold",
         title: "🫖 Handmade Teapot",
         subtitle:
-          "An artisanal \"easy gaiwan\" (pictured above) made by local ceramicist Lilian Wu.",
+          "An artisanal \"easy gaiwan\" (pictured above) made by local ceramicist Lilian Wu. Each gaiwan is unique!",
       },
       {
         state: "bold",
