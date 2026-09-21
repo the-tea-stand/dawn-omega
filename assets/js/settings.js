ad2af587d58bf6a36548586d330c548ef80db753
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2026-09-21 15:11:09 -0400

    4x year

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -183,7 +183,7 @@ let membershipTiers = [
         state: "bold",
         title: "📗 bōcha: our quarterly zine",
         subtitle:
-          "Every issue of our riso-printed zine delivered to your door. Published every solstice + equinox (4x/year). Created by many minds, folded by many hands.",
+          "Every issue of our riso-printed zine delivered to your door. Published 4x/year on the solstices + equinoxes. Created by many minds, folded by many hands.",
       },
       {
         state: "bold",
