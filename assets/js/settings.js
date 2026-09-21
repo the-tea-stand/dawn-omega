d9b6cbf364c7e9d0439270ed7c83cfe19cbd6dcc
mkirsch17 <37664884+mkirsch17@users.noreply.github.com>
2026-09-21 15:31:27 -0400

    yah
    Updated subtitle for Handmade Teapot entry for clarity.

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -256,7 +256,7 @@ let membershipTiers = [
         state: "bold",
         title: "🫖 Handmade Teapot",
         subtitle:
-          "An artisanal \"easy gaiwan\" teapot made by local ceramicist Lilian Wu. Example photo above, though each one is unique!",
+          "An artisanal \"easy gaiwan\" teapot made by local ceramicist Lilian Wu (example pictured above). Each teapot is unique!",
       },
       {
         state: "bold",
